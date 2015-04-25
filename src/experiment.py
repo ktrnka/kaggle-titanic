b10076ca9910a0010ed6597313b7303d8d52207b
Keith Trnka <keith.trnka@gmail.com>
2015-04-24 17:01:29 -0700

    Cleaning it up, showing the weights

--- src/experiment.py
+++ src/experiment.py
@@ -6,8 +6,10 @@ import numpy
 import sklearn.ensemble
 import sklearn.cross_validation
 import sklearn.linear_model
+import sklearn.preprocessing
 
 sex_map = {"female": 1, "male": 0}
+
 
 def transform_features(data, ages):
     data["SexNum"] = data.Sex.map(sex_map).astype(int)
@@ -19,9 +21,10 @@ def transform_features(data, ages):
 
     for sex in xrange(2):
         for pass_class in xrange(3):
-            data.loc[(data.Age.isnull()) & (data.SexNum == sex) & (data.Pclass == pass_class + 1), "AgeFill"] = ages[sex, pass_class]
+            mask = (data.Age.isnull()) & (data.SexNum == sex) & (data.Pclass == pass_class + 1)
+            data.loc[mask, "AgeFill"] = ages[sex, pass_class]
 
-    # data["AgeIsNull"] = data.Age.isnull().astype(int)
+    #data["AgeIsNull"] = data.Age.isnull().astype(int)
     data["FamilySize"] = data.SibSp + data.Parch
     #data["Age*Class"] = data.AgeFill * data.Pclass
 
@@ -36,8 +39,8 @@ def build_age_matrix(data):
     reverse_sex_map = {val: key for key, val in sex_map.iteritems()}
     for sex in xrange(2):
         for pass_class in xrange(3):
-            ages[sex, pass_class] = data[(data.Sex == reverse_sex_map[sex]) & (data.Pclass == pass_class + 1)].Age.dropna().median()
-            data.loc[(data.Age.isnull()) & (data.Sex == reverse_sex_map[sex]) & (data.Pclass == pass_class + 1), "AgeFill"] = ages[sex, pass_class]
+            mask = (data.Sex == reverse_sex_map[sex]) & (data.Pclass == pass_class + 1)
+            ages[sex, pass_class] = data[mask].Age.dropna().median()
     return ages
 
 
@@ -49,7 +52,7 @@ data = transform_features(data, ages)
 training_data = data.values
 
 # classifier = sklearn.ensemble.RandomForestClassifier(1000, max_features=0.8, min_samples_split=50)
-classifier = sklearn.linear_model.LogisticRegression(penalty="l2", C=10.)
+classifier = sklearn.linear_model.LogisticRegression(penalty="l2", C=0.1)
 
 # cross-validate the classifier
 cv_scores = sklearn.cross_validation.cross_val_score(classifier, training_data[:, 1:], training_data[:, 0], cv=10)
@@ -62,13 +65,17 @@ training_predictions = classifier.predict(training_data[:, 1:])
 diffs = training_predictions - training_data[:, 0]
 print "Training error", numpy.abs(diffs).mean()
 
+print data.info()
+print "Weights", zip(data.columns[1:].values, classifier.coef_[0,:])
+
+
 test_data = pandas.read_csv("../data/test.csv", header=0)
 ids = test_data.PassengerId.values
 test_data = transform_features(test_data, ages)
 
 test_predictions = classifier.predict(test_data.values)
 
-with io.open("../data/forest3.csv", "wb") as csv_out:
+with io.open("../data/logistic1.csv", "wb") as csv_out:
     csv_writer = csv.writer(csv_out)
     csv_writer.writerow(["PassengerId", "Survived"])
     csv_writer.writerows(zip(ids, test_predictions.astype(int)))
